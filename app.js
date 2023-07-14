4be03596f9eff0d90069df6fc164bb7fea3cbb72
Vantoss <80919590+Vantoss@users.noreply.github.com>
2023-07-14 10:53:08 -0300

    code cleanup

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const mysql = require("mysql");
 const path = require("path");
 const passport = require("passport");
 
-// Importar o modelo de usuário
-//const User = require("./models/user");
-
 // Criar uma conexão com o banco de dados MySQL
 const db = mysql.createConnection({
   host: "localhost",
@@ -61,11 +58,8 @@ passport.deserializeUser(function (id, cb) {
 // Definir a porta do servidor
 const PORT = 3000;
 
-// Criar uma rota GET para listar todos os furtos de bicicleta
 app.get("/api/furtos", (req, res) => {
-  // Criar uma query SQL para selecionar todos os registros da tabela furto_bicicleta
   let sql = "SELECT * FROM furto_bicicleta";
-  // Executar a query e enviar a resposta
   db.query(sql, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -75,13 +69,9 @@ app.get("/api/furtos", (req, res) => {
   });
 });
 
-// Criar uma rota GET para buscar um furto de bicicleta pelo id
 app.get("/api/furtos/:id", (req, res) => {
-  // Obter o id do parâmetro da rota
   let id = req.params.id;
-  // Criar uma query SQL para selecionar o registro da tabela furto_bicicleta com o id informado
   let sql = `SELECT * FROM furto_bicicleta WHERE id = ${id}`;
-  // Executar a query e enviar a resposta
   db.query(sql, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -95,15 +85,10 @@ app.get("/api/furtos/:id", (req, res) => {
   });
 });
 
-// Criar uma rota POST para inserir um novo furto de bicicleta
 app.post("/api/furtos", (req, res) => {
-  // Obter os dados do corpo da requisição
   let data = req.body;
-  // Validar se os dados estão completos
   if (data.marca && data.modelo && data.horario && data.local && data.latitude && data.longitude) {
-    // Criar uma query SQL para inserir um novo registro na tabela furto_bicicleta com os dados informados
     let sql = `INSERT INTO furto_bicicleta (marca, modelo, horario, local, latitude, longitude) VALUES ('${data.marca}', '${data.modelo}', '${data.horario}', '${data.local}', ${data.latitude}, ${data.longitude})`;
-    // Executar a query e enviar a resposta
     db.query(sql, (err, result) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -119,17 +104,11 @@ app.post("/api/furtos", (req, res) => {
   }
 });
 
-// Criar uma rota PUT para atualizar um furto de bicicleta pelo id
 app.put("/api/furtos/:id", (req, res) => {
-  // Obter o id do parâmetro da rota
   let id = req.params.id;
-  // Obter os dados do corpo da requisição
   let data = req.body;
-  // Validar se os dados estão completos
   if (data.marca && data.modelo && data.horario && data.local && data.latitude && data.longitude) {
-    // Criar uma query SQL para atualizar o registro da tabela furto_bicicleta com o id informado e os dados informados
     let sql = `UPDATE furto_bicicleta SET marca = '${data.marca}', modelo = '${data.modelo}', horario = '${data.horario}', local = '${data.local}', latitude = ${data.latitude}, longitude = ${data.longitude} WHERE id = ${id}`;
-    // Executar a query e enviar a resposta
     db.query(sql, (err, result) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -146,13 +125,9 @@ app.put("/api/furtos/:id", (req, res) => {
   }
 });
 
-// Criar uma rota DELETE para remover um furto de bicicleta pelo id
 app.delete("/api/furtos/:id", (req, res) => {
-  // Obter o id do parâmetro da rota
   let id = req.params.id;
-  // Criar uma query SQL para deletar o registro da tabela furto_bicicleta com o id informado
   let sql = `DELETE FROM furto_bicicleta WHERE id = ${id}`;
-  // Executar a query e enviar a resposta
   db.query(sql, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -164,31 +139,6 @@ app.delete("/api/furtos/:id", (req, res) => {
       }
     }
   });
-});
-
-// Criar uma rota GET para autenticar o usuário com o GitHub
-app.get("/auth/github", passport.authenticate("github"));
-
-// Criar uma rota GET para receber o callback do GitHub após a autenticação
-app.get("/github/callback", passport.authenticate("github", { failureRedirect: "/login" }), function (req, res) {
-  // Autenticação bem-sucedida, redirecionar para a página admin.html
-  res.redirect("/admin.html");
-});
-
-// Criar um middleware para verificar se o usuário está autenticado
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    // Se o usuário está autenticado, prosseguir com a próxima função
-    return next();
-  }
-  // Se o usuário não está autenticado, redirecionar para a página index.html
-  res.redirect("/index.html");
-}
-
-// Usar o middleware para proteger a rota que envia o arquivo admin.html
-app.get("/admin.html", ensureAuthenticated, (req, res) => {
-  // Enviar o arquivo admin.html como resposta
-  res.sendFile(path.join(__dirname, "admin.html"));
 });
 
 // Iniciar o servidor na porta definida
--- public/admin.js
+++ public/admin.js
@@ -5,13 +5,9 @@ async function verify() {
   const ticket = await client.verifyIdToken({
       idToken: token,
       audience: "980205035441-gmvj389h1q3tg6rg1b74bpi7fbr4686i.apps.googleusercontent.com",  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
   const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
 }
 verify().catch(console.error);
 
